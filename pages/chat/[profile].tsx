a1da0ae4e98bede897f8f3a3d752877e60425036
Mate Fater <mate.fater@gmail.com>
2025-05-25 00:18:51 +0200

    chatpage update fixed

--- pages/chat/[profile].tsx
+++ pages/chat/[profile].tsx
@@ -180,24 +180,6 @@ export default function ChatPage() {
       </button>
     )}
 
-<button className="reflecta-close-button" aria-label="Zárás">
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="20"
-    height="20"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  >
-    <path d="M18 6L6 18" />
-    <path d="M6 6l12 12" />
-  </svg>
-</button>
-
-
     <button
       className={`reflecta-send-button ${loading ? 'reflecta-send-loading' : ''}`}
       onClick={() => handleSend()}
