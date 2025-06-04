27d2684936ebb0c615b2a26e1397a330f91d2160
Mate Fater <mate.fater@gmail.com>
2025-06-04 19:55:04 +0200

    preferences panel icons updated

--- components/PreferencesPanel.tsx
+++ components/PreferencesPanel.tsx
@@ -256,8 +256,8 @@ export function PreferencesPanel({
             className={styles.resetButton}
           >
             <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-  <path d="M3 12h18" />
-  <path d="M9 6l-6 6 6 6" />
+  <polyline points="1 4 1 10 7 10" />
+  <path d="M3.51 15a9 9 0 1 1 2.13 3.13" />
 </svg>
             Visszaállítás
           </button>
