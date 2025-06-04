fa9208d49bf13ccd762e13188839400425e1b62a
Mate Fater <mate.fater@gmail.com>
2025-06-04 19:59:21 +0200

    preferences panel icons updated

--- components/PreferencesPanel.tsx
+++ components/PreferencesPanel.tsx
@@ -152,8 +152,11 @@ export function PreferencesPanel({
   label: 'Csendesítő',
   value: 'soothing',
   icon: (
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
-      <path d="M12 2c0 3.5-2 5-2 5s-2-1.5-2-5c-3.5 3-3 7-3 7s-3 0-3 3c0 4 8 9 10 9s10-5 10-9c0-3-3-3-3-3s.5-4-3-7c0 3.5-2 5-2 5s-2-1.5-2-5z" />
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+      <circle cx="12" cy="12" r="3" />
+      <path d="M12 16.5A4.5 4.5 0 1 1 7.5 12 4.5 4.5 0 1 1 12 7.5a4.5 4.5 0 1 1 4.5 4.5 4.5 4.5 0 1 1-4.5 4.5" />
+      <path d="M12 7.5V9" /><path d="M7.5 12H9" /><path d="M16.5 12H15" /><path d="M12 16.5V15" />
+      <path d="m8 8 1.88 1.88" /><path d="M14.12 9.88 16 8" /><path d="m8 16 1.88-1.88" /><path d="M14.12 14.12 16 16" />
     </svg>
   )
 }
